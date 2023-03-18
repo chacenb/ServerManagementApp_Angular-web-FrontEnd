78816af8013b7bc25262243be1c1ed08a1340502
chace <chacenb@gmail.com>
2023-03-18 13:30:30 +0100

    ...

--- serverManagement-ngFrontEnd/src/app/app-routing.module.ts
+++ serverManagement-ngFrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "sm" }, // Default Route
+  {
+    path: 'sm', children: [
+      { path: '', loadChildren: () => import('./features/features.module').then(m => m.FeaturesModule) }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
