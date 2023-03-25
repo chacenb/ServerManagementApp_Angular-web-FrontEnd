47ad7c60d3f52e52613098be1d2f807d597d9f87
chace <chacenb@gmail.com>
2023-03-25 09:40:09 +0100

    ...

--- serverManagement-ngFrontEnd/src/app/features/features.component.ts
+++ serverManagement-ngFrontEnd/src/app/features/features.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { map, Observable, startWith } from 'rxjs';
+import { catchError, map, Observable, of, startWith } from 'rxjs';
 import { ServerService } from '../core/services/apiService/server.service';
 import * as mod from '../core/models/models';
 
@@ -17,14 +17,23 @@ export class FeaturesComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+
+    /* building the application state Observable to be used */
     this.appState$ = this.serverService.get_all_servers$.pipe(
       map((response) => {
+        /* build an object of type AppState<CustomResponse> to return*/
         const tempAppState: mod.AppState<mod.CustomResponse> = { dataState: mod.DataState.LOADED_STATE, appData: response };
         return tempAppState;
       }),
 
       /* set initial value while waiting for the http request to complete and provide us w/ the response */
       startWith({ dataState: mod.DataState.LOADING_STATE }),
+
+      catchError((error: string) => {
+        /* build an object of type AppState<CustomResponse> */
+        const tempErrState: mod.AppState<mod.CustomResponse> = { dataState: mod.DataState.ERROR_STATE, error };
+        return of(tempErrState);
+      }),
 
     )
   }
