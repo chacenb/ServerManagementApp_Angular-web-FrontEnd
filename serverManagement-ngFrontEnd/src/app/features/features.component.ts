aebb07cb0858cb34f0458bfc49845dccd918cef0
chace <chacenb@gmail.com>
2023-03-27 02:36:18 +0100

    Frontend reactive approch ongoing

--- serverManagement-ngFrontEnd/src/app/features/features.component.ts
+++ serverManagement-ngFrontEnd/src/app/features/features.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { catchError, delay, map, Observable, of, startWith } from 'rxjs';
+import { BehaviorSubject, catchError, delay, map, Observable, of, startWith } from 'rxjs';
 import { ServerService } from '../core/services/apiService/server.service';
 import * as mod from '../core/models/models';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -12,6 +12,14 @@ import { HttpErrorResponse } from '@angular/common/http';
 export class FeaturesComponent implements OnInit {
 
   appState$: Observable<mod.AppState<mod.CustomResponse>>;
+  readonly dataStates = mod.DataState;
+  readonly Statuses = mod.Status;
+
+  private pinging_ = new BehaviorSubject<number>(-1); // same as new ReplaySubject<boolean>(1);
+  pinging$ = this.pinging_.asObservable();
+
+  pinging: boolean = false;
+  pingedServer: number = -1
 
   constructor(
     private serverService: ServerService
@@ -34,7 +42,7 @@ export class FeaturesComponent implements OnInit {
       }),
 
       /* delay the return of the response */
-      delay(2000),
+      // delay(2000),
 
       /* set initial value while waiting for the http request to complete and provide us w/ the response */
       startWith({ dataState: mod.DataState.LOADING_STATE }),
@@ -46,7 +54,18 @@ export class FeaturesComponent implements OnInit {
         return of(tempErrState);
       }),
 
-    )
+    );
+  }
+
+
+  pingServer(id: number) {
+    this.pinging_.next(id);
+    setTimeout(() => { this.pinging_.next(-1); }, 2000);
+  }
+
+  addNewServer() {
+    console.log('ADDING new server');
+
   }
 
 }
